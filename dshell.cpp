540ad492cedafa209bd7a049204e0b8a74e23da3
Dima Brusilovsky <dimabru@gmail.com>
2018-03-31 14:04:10 +0300

    Finished task1 in ex2

--- dshell.cpp
+++ dshell.cpp
@@ -7,10 +7,11 @@
 #include <list>
 #include <algorithm>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 using namespace std;
 
-int signal_value = 0;
+int exit_status = 0;
 
 string replaceHome(string cwd);
 bool commandAnalyse(string command);
@@ -20,7 +21,8 @@ string getCommandName(string str);
 bool knownCommand(string command);
 
 string eval_word(string word);
-string eval_path(string path);
+string eval_dollar(string, unsigned int&);
+string eval_path(string);
 
 int main()
 {
@@ -34,18 +36,11 @@ int main()
             return 0;
         }
         string cwd = getcwd(NULL, 0);
-
+        
         cwd = replaceHome(cwd);
 
         cout << "OS Shell: " << cwd << " >> " << flush;
         
-        signal(SIGINT, signal_handler);
-        // activation of control+c
-        if (signal_value == 2) 
-        {
-            signal_value = 0;
-            continue;
-        }
         string comm;
         getline(cin, comm);
         if (comm.empty()) comm = "";
@@ -57,59 +52,86 @@ int main()
     }
 }
 
-// Evaluate a single word and eliminate environment variables. 
+// Evaluate a single word (and path) and eliminate environment variables. 
 // Send only one word each use!!
 string eval_word(string word)
 {
     string to_return = "";
+
+    for (unsigned int i = 0; i < word.size(); i++)
+    {
+        if (word[i] == '$')
+            to_return += eval_dollar(word.substr(i + 1, word.size()), i);
+        else to_return += word[i];
+    }
+
+    return to_return;
+}
+
+// Utility function. Do not use it
+string eval_dollar(string word, unsigned int &index)
+{
     string env_var = "";
+    
+    if (word[0] == '?')
+    {
+        index++;
+        return to_string(exit_status);
+    }
+
     for (unsigned int i = 0; i < word.size(); i++)
     {
         if (word[i] == '$')
         {
-            while (i + 1 < word.size() && word[i + 1] == '$') i++;
-            while (++i < word.size())
+            if (env_var[0] == '\0') return "$";
+            else
             {
-                if (word[i] == '$')
-                {
-                    const char *cast = env_var.c_str();
-                    if (env_var != "") to_return += getenv(cast);
-                    env_var = "";
-                    break;
-                }
-                env_var += word[i++];
+                index += env_var.size();
+                const char *cast = env_var.c_str();
+                const char *to_return = getenv(cast);
+                if (!to_return) return "";
+                return string(to_return);
             }
         }
-        else 
-        {
-            to_return += word[i];
-        }
+        else env_var += word[i];
     }
-    const char *cast = env_var.c_str();
-    if (env_var != "") to_return += getenv(cast);
-    return to_return;
+    if (env_var[0] == '\0') return "$";
+    else
+    {
+        index += env_var.size();
+        const char *cast = env_var.c_str();
+        const char *to_return = getenv(cast);
+        if (!to_return) return "";
+        return string(to_return);
+    }
 }
 
-// Signal handler. Should be called whenever we want to address a user signal
-void signal_handler(int sig)
+// Send every path here to evaluate it and replace all variables (if exist)
+// and replace home directory with ~ if necessary
+string eval_path(string path)
 {
-   signal_value = sig;
-   if (sig == SIGINT)
-   {
-       cout << endl;
-   }
+    string to_return = "";
+    if (path[0] == '~') 
+    {
+        to_return += (getpwuid(getuid()))->pw_dir;
+        to_return += eval_word(path.substr(1, path.size()));
+    }
+    else to_return += eval_word(path);
+
+    return to_return;
 }
 
 // Analysing every command string. This function should be called recursively on every new command string
 bool commandAnalyse(string str)
 {
     string cmd = getCommandName(str);
-    cout << eval_word(cmd) << endl;
-    if (!knownCommand(cmd))
+   
+    /*if (!knownCommand(cmd))
     {
-        cout << str << ": No such command" << endl;
-    }
-    else if (str == "exit")
+        exit_status = 127;
+        cout << cmd << ": No such command" << endl;
+    }*/
+    if (cmd == "exit")
     {
         cout << "You chose to quit shell, good day" << endl;
         return false;
@@ -117,42 +139,46 @@ bool commandAnalyse(string str)
     else if (cmd=="") { }
     else if (cmd == "cd")
     {
-	    list<string> tokens = tokenize(str);
-        list<string>::iterator it = tokens.begin();
-        if(tokens.size()==2)
-        {
-            string newDir;
-            newDir = getcwd(NULL, 0);
-            advance(it,1);
-            if((*it)[0]=='~')
-            {
-                string tempStr = (getpwuid(getuid()))->pw_dir;
-                tempStr += '/';
-                for (unsigned int i = 2; i < (*it).size(); i++)
-                {
-                    tempStr += (*it)[i];
-                }
-                newDir = tempStr;
-            }
-            else
-                newDir+="/" + *it;
-            if(chdir(newDir.c_str())!=0)
-            {
-                cout<<"Directory '/"+*it+"' does not exist"<<endl;
-            }
-        }
-        else if(tokens.size()==1){}
-        else
-            cout<<"Wrong input"<<endl;
-    }
-    else 
-    {
         list<string> tokens = tokenize(str);
         list<string>::iterator it = tokens.begin();
-        for (unsigned int i=0; i < tokens.size(); i++)
+        if(tokens.size() == 1) 
         {
-            cout << *it << endl;
+            string homedir = (getpwuid(getuid()))->pw_dir;
+            exit_status = chdir(homedir.c_str());
+        }
+        else if (tokens.size() == 2)
+        {
             advance(it,1);
+            string path = eval_path(*it);
+            exit_status = chdir(path.c_str());
+            if (exit_status != 0)
+            {
+                exit_status = 1;
+                cout << "Directory " + path + " could not be located" << endl;
+            }
+        }
+        else 
+        {
+            exit_status = 1;
+            cout << "cd: Too many arguments" << endl;
+        }
+    }
+    else
+    {
+        pid_t kidpid = fork();
+        if (kidpid == 0)
+        {
+            char *args[2];
+            string arg = "-la";
+            args[0] = (char*)arg.c_str();
+            args[1] = NULL;
+            execvp("ls", args);
+        }
+        else if (kidpid > 0)
+        {
+            if (waitpid(kidpid, 0, 0) < 0)
+            {
+            }
         }
     }
     return true;
@@ -164,7 +190,7 @@ string getCommandName(string str)
     list<string> tokens = tokenize(str);
     list<string>::iterator it = tokens.begin();
     if (tokens.size() == 0) return "";
-    return *it;
+    return eval_word(string(*it));
 }
 
 // Returns true if a command is known and can be used, else returns false
@@ -221,8 +247,6 @@ string replaceHome(string cwd)
     }
     return cwd;
 }
-
-
 
 
 
