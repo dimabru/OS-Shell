4eb738a6465683b9c61092f2f1c15b7df58f68b0
dimabru@gmail.com <dimabru@gmail.com>
2018-04-21 17:52:57 +0300

    Started working on shared memory

--- ex3.cpp
+++ ex3.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <iomanip>
 #include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
 
 using namespace std;
 
@@ -21,15 +24,19 @@ typedef struct Order
     bool done = true;
 } Order;
 
+#define MENUSIZE sizeof(struct MenuItem)
+#define ORDERSIZE sizeof(struct Order)
+
 bool checkArguments(int, int, int, int);
-void printMenu(MenuItem*);
+void printMenu(MenuItem*, int);
 
 int main(int args_size, char *args[])
 {
+
     if (args_size != 5) 
     {
         fprintf(stderr, "Input arguments are not valid\n");
-        return -1;
+        exit(1);
     }
     int simulation_time = atoi(args[1]);
     int menu_items_amount = atoi(args[2]);
@@ -39,7 +46,7 @@ int main(int args_size, char *args[])
     if (!checkArguments(simulation_time, menu_items_amount, customers_count, waiters_count))
     {
         fprintf(stderr, "Input arguments are not valid\n");
-        return -1;
+        exit(1);
     }
 
     MenuItem items[] = {
@@ -48,10 +55,40 @@ int main(int args_size, char *args[])
         {2, "Hamburger", 12, 0},
         {3, "Spaghetti", 9, 0},
         {4, "Pie", 9.5, 0},
-        {5, "Milkshake", 6, 0}
+        {5, "Milkshake", 6, 0},
+        {6, "Shwarma", 11, 0},
+        {7, "Sushi", 15, 0},
+        {8, "Kuskus", 12, 0},
+        {9, "Peanuts", 5, 0},
     };
 
-    printMenu(items);
+    // Key of the menu
+    key_t menu_key = ftok(".", 'M');
+    int shmid;
+    char *shm;
+    MenuItem *shm_menu;
+    
+    if (shmid = shmget(menu_key, MENUSIZE * 100, 0666) < 0)
+    {
+        perror("shmget");
+		exit(1);
+    }
+    if ((shm = (char *)shmat(shmid, NULL, 0)) == (char *)-1)
+    {
+        perror("shmat");
+		exit(1);
+    }
+    shm_menu = (MenuItem*)shm;
+    for (int i=0; i<menu_items_amount; i++)
+    {
+        *shm_menu++ = items[i];
+    }
+    shm_menu = (MenuItem*)shm;
+    printMenu(shm_menu, menu_items_amount);
+
+    // Key of the orders
+    key_t order_key = ftok(".", 'O');
+    
 
     return 0;
 }
@@ -69,14 +106,14 @@ bool checkArguments(int sim, int menu_items, int cust, int waiters)
     return true;
 }
 
-void printMenu(MenuItem items[])
+void printMenu(MenuItem items[], int size)
 {
     cout << setw(4) << left << "Id";
     cout << setw(15) << left << "Name";
     cout << setw(7) << left << "Price";
     cout << "Orders" << endl;
 
-    for (int i=0; i < 6; i++)
+    for (int i=0; i < size; i++)
     {
         cout << setw(4) << left << items[i].id; 
         cout << setw(15) << left << items[i].name;
